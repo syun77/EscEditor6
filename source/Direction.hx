5ae35a379c64f78100388f155a37d093181f4c6a
Syunsuke Ozeki <2dgames.jp@gmail.com>
2019-08-04 23:22:29 +0900

    移動カーソル実装中

---
+++ source/Direction.hx
@@ -0,0 +1,16 @@
+package;
+
+enum Dir {
+    Left;
+    Up;
+    Right;
+    Down;
+}
+
+class Direction {
+    public static inline var LEFT:Int  = 0;
+    public static inline var UP:Int    = 1;
+    public static inline var RIGHT:Int = 2;
+    public static inline var DOWN:Int  = 3;
+    public static inline var MAX:Int   = 4;
+}
---
+++ source/ui/MovingCursorUI.hx
@@ -0,0 +1,8 @@
+package ui;
+
+import flixel.FlxSprite;
+import flixel.group.FlxSpriteGroup;
+
+class MovingCursorUI extends FlxSpriteGroup {
+    var _cursors:Array<FlxSprite>;
+}
