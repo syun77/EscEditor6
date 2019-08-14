c6adf2338e593ec759c49b51c9790c4c8f9ef46c
Syunsuke Ozeki <2dgames.jp@gmail.com>
2019-08-14 12:12:53 +0900

    通知メッセージの背景表示を実装

--- source/ui/InformationUI.hx
+++ source/ui/InformationUI.hx
@@ -6,19 +6,35 @@ import flixel.FlxSprite;
 import flixel.text.FlxText;
 import flixel.tweens.FlxTween;
 import flixel.tweens.FlxEase;
+import flixel.util.FlxColor;
 
 class InformationUI extends FlxSpriteGroup {
 
+    static inline var HEIGHT:Int = 40;
+
+    var _bg:FlxSprite;
     var _txt:FlxText;
     var _tween:FlxTween = null;
 
     public function new() {
         super();
 
+        // 背景
+        {
+            var h = HEIGHT + 20;
+            _bg = new FlxSprite(0, FlxG.height-h).makeGraphic(FlxG.width, h, FlxColor.WHITE);
+            _bg.color = FlxColor.BLACK;
+            _bg.alpha = 0.5;
+            this.add(_bg);
+        }
+
+        // テキスト
         _txt = new FlxText(0, FlxG.height-40, FlxG.width, null);
         _txt.setFormat(Resources.FONT_PATH, Resources.FONT_SIZE);
         _txt.alignment = FlxTextAlign.CENTER;
         this.add(_txt);
+
+        visible = false;
     }
 
     override public function update(elapsed:Float):Void {
@@ -32,9 +48,9 @@ class InformationUI extends FlxSpriteGroup {
         }
 
         _txt.text = msg;
-        _txt.visible = true;
+        visible = true;
         _tween = FlxTween.tween(_txt, {}, time, {onComplete:function(_) {
-            _txt.visible = false;
+            visible = false;
             _tween = null;
         }});
     }
