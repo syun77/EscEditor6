4f33e872d8b79f235156bd3ea4883f4372ff6cad
Syunsuke Ozeki <2dgames.jp@gmail.com>
2019-08-18 17:00:04 +0900

    リリース時はタイトル画面から始まるようにした

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,10 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+#if debug
 		addChild(new FlxGame(0, 0, PlayState, 1, 60, 60, true));
+#else
+		addChild(new FlxGame(0, 0, TitleState, 1, 60, 60, true));
+#end
 	}
 }
