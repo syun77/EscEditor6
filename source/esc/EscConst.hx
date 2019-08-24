d53dde4e903ac69a7cceeec1ed41e0b6589f40ea
Syunsuke Ozeki <2dgames.jp@gmail.com>
2019-08-24 19:46:51 +0900

    アイテム追加

--- source/esc/EscConst.hx
+++ source/esc/EscConst.hx
@@ -8,4 +8,9 @@ class EscConst {
 	public static inline var ITEM_BLUE_KEY:Int = 3;
 	public static inline var ITEM_GREEN_KEY:Int = 4;
 	public static inline var ITEM_GLASS:Int = 5;
+	public static inline var ITEM_FLOPPY:Int = 6;
+	public static inline var ITEM_FLASHLIGHT_NA:Int = 7;
+	public static inline var ITEM_BATTERY:Int = 8;
+	public static inline var ITEM_REMOTE:Int = 9;
+	public static inline var ITEM_FLASHLIGHT:Int = 10;
 }
