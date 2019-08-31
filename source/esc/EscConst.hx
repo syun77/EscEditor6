c8fb3bde35108706f75e8abed989781a06fb6fcd
Syunsuke Ozeki <2dgames.jp@gmail.com>
2019-08-31 19:50:35 +0900

    ゲートEを実装

--- source/esc/EscConst.hx
+++ source/esc/EscConst.hx
@@ -7,6 +7,8 @@ class EscConst {
 	public static inline var GateB:Int = 3;
 	public static inline var GateC:Int = 4;
 	public static inline var GateD:Int = 5;
+	public static inline var GateE:Int = 6;
+	public static inline var GateE2:Int = 7;
 	public static inline var ITEM_DUMMY:Int = 0;
 	public static inline var ITEM_YELLOW_KEY:Int = 1;
 	public static inline var ITEM_RED_KEY:Int = 2;
