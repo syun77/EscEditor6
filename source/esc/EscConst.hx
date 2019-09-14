55062142dbdefc135b577a0e0805050e4bcaabed
Syunsuke Ozeki <syun@SyunsukenoMac-mini.local>
2019-09-14 13:47:58 +0900

    不要なファイルを削除

--- source/esc/EscConst.hx
+++ source/esc/EscConst.hx
@@ -3,11 +3,6 @@ package esc;
 class EscConst {
 	public static inline var TEST_CONST:Int = 0;
 	public static inline var Entrance:Int = 1;
-	public static inline var PrisonWest:Int = 2;
-	public static inline var Toilet:Int = 3;
-	public static inline var Sink:Int = 4;
-	public static inline var Bed:Int = 5;
-	public static inline var Desk:Int = 6;
 	public static inline var ITEM_DUMMY:Int = 0;
 	public static inline var ITEM_STICK:Int = 1;
 	public static inline var ITEM_HANDLE:Int = 2;
