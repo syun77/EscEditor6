bc0d10ca069a14ff2ba5ec7f184f6567b3001890
Syunsuke Ozeki <syun@SyunsukenoMac-mini.local>
2019-09-14 17:48:23 +0900

    3号車両追加

--- source/esc/EscConst.hx
+++ source/esc/EscConst.hx
@@ -3,6 +3,8 @@ package esc;
 class EscConst {
 	public static inline var TEST_CONST:Int = 0;
 	public static inline var Entrance:Int = 1;
+	public static inline var Train2:Int = 2;
+	public static inline var Train3:Int = 3;
 	public static inline var ITEM_DUMMY:Int = 0;
 	public static inline var ITEM_STICK:Int = 1;
 	public static inline var ITEM_HANDLE:Int = 2;
