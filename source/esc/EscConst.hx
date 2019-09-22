8897d4500dd864421299066c27922df80ca5e038
Syunsuke Ozeki <syun@SyunsukenoMac-mini.local>
2019-09-22 20:57:07 +0900

    ドライバーの配置を実装

--- source/esc/EscConst.hx
+++ source/esc/EscConst.hx
@@ -8,10 +8,14 @@ class EscConst {
 	public static inline var Room:Int = 4;
 	public static inline var Examination:Int = 5;
 	public static inline var ITEM_DUMMY:Int = 0;
-	public static inline var ITEM_STRAP_CIRCLE:Int = 1;
-	public static inline var ITEM_STRAP_TRIANGLE:Int = 2;
-	public static inline var ITEM_MEMO:Int = 3;
+	public static inline var ITEM_CELL_PHONE:Int = 1;
+	public static inline var ITEM_DRIVER:Int = 2;
+	public static inline var ITEM_BATTERY:Int = 3;
 	public static inline var ITEM_KNIFE:Int = 4;
-	public static inline var ITEM_FLOPPY:Int = 5;
-	public static inline var ITEM_MASK:Int = 6;
+	public static inline var ITEM_STICK:Int = 5;
+	public static inline var ITEM_FIRE_STICK:Int = 6;
+	public static inline var ITEM_NOTE1:Int = 7;
+	public static inline var ITEM_NOTE2:Int = 8;
+	public static inline var ITEM_WET_NOTE1:Int = 9;
+	public static inline var ITEM_WET_NOTE2:Int = 10;
 }
